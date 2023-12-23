c677d20efb3ae44bfe9495cb36e00563dd9b95bd
Intellimello <36728792+Intelimello@users.noreply.github.com>
2023-12-23 22:45:39 +0530

    Flutter Boring Show:1 - added a dummy list to the screen

---
+++ lib/src/articles.dart
@@ -0,0 +1,99 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article({
+    required this.text,
+    required this.by,
+    required this.age,
+    required this.commentsCount,
+    required this.domain,
+    required this.score,
+  });
+}
+
+final articles = [
+  const Article(
+    text: "Circuook like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 10,
+  ),
+  const Article(
+    text:
+        "Circular Ri Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 20,
+  ),
+  const Article(
+    text:
+        "Circular Rings look like a e a circle Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 30,
+  ),
+  const Article(
+    text:
+        "Circular e Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 12,
+  ),
+  const Article(
+    text: "cular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 17,
+  ),
+  const Article(
+    text:
+        " like a circle Circular Rings look like a circle Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 10,
+  ),
+  const Article(
+    text:
+        "a circle Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 70,
+  ),
+  const Article(
+    text:
+        "Circular Rings look like a circle Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 19,
+  ),
+  const Article(
+    text:
+        "Circular Rings look like a circle Circular Rings look like a circle Circular Rings look like a circle Circular Rings look like a circle",
+    domain: "www.circularing.com",
+    by: "",
+    age: "",
+    score: 10,
+    commentsCount: 10,
+  ),
+];
